d6e7e9a0dea82b16d477a488cfec4e7cf7abe055
Julien Jamme <julien.jamme@protonmail.com>
2023-06-30 01:12:20 +0000

    quadtree and smoothing tests

--- R/sdcSpatial_experiment_case_of_La_Reunion.R
+++ R/sdcSpatial_experiment_case_of_La_Reunion.R
@@ -7,10 +7,13 @@ library(dplyr)
 library(SpatialKWD)
 library(sdcSpatial)
 library(data.table)
+library(mapview)
 
 # download.file("https://www.insee.fr/fr/statistiques/fichier/6215140/Filosofi2017_carreaux_1km_gpkg.zip", "filo2017_1km_gpkg.zip")
 # download.file("https://www.insee.fr/fr/statistiques/fichier/6215140/Filosofi2017_carreaux_200m_gpkg.zip", "filo2017_200m_gpkg.zip")
+borders_mun_sf <- sf::st_read("data/commune_dep_974_2019.gpkg")
 pop_grid_200m_sf <- sf::st_read("data/Filosofi2017_carreaux_200m_reun.gpkg")
+pop_grid_nat_sf <- sf::st_read("data/Filosofi2017_carreaux_nivNaturel_reun.gpkg")
 str(pop_grid_200m_sf)
 
 centroides <- sf::st_centroid(pop_grid_200m_sf)
@@ -36,18 +39,57 @@ pop_200m <- pop_grid_200m_sf %>%
 
 ggplot(pop_200m) +
   geom_sf(aes(fill = Men), color = NA) +
+  geom_sf(data = borders_mun_sf, fill = NA) +
   scale_color_viridis_c(alpha = 0.75)
 
+
+qt <- quantile(pop_200m$Men, probs = seq(0,1,0.1))
+qt[1] <- 0
+pal <- c("#FDE333", "#BBDD38", "#6CD05E", "#00BE7D", "#00A890"
+         , "#008E98",  "#007094", "#185086", "#422C70", "#4B0055")
+
+mapview(
+  borders_mun_sf,
+  col.regions = NA,
+  color = c("grey15"),
+  alpha = 0.95,
+  alpha.regions = 0,
+  map.types = c("OpenStreetMap","Esri.WorldImagery")
+) + mapview(
+  pop_200m, 
+  z = c("Men"),
+  at = qt,
+  lwd = 0,
+  color.regions = pal,
+  alpha.regions = 0.65,
+  layer.name = "Households"
+) +
+  mapview(
+  pop_grid_nat_sf, 
+  z = c("Men"),
+  # at = qt,
+  lwd = 0.3,
+  color.regions = pal,
+  alpha.regions = 0.35,
+  na.color = NA,
+  layer.name = "Natural Level"
+)
+#The quadtree is very sensitive to the "origin of the grid"
+#Side effect of quadtree method applied on Inspire nested grid layers (200m,1km,2km,etc.)
+
+
+summary(pop_200m$Men)
+table(pop_200m$Men < 11)
 
 hh_200m <- pop_200m %>% 
   sf::st_drop_geometry() %>% 
   select(IdInspire, Men) %>% 
-  mutate(Men = round(Men)) %>% 
+  mutate(Men = ceiling(Men)) %>% 
   as.data.table()
 
 hh_200m <- hh_200m[rep(seq(.N), Men), ]
 hh_200m[, id := 1:.N]
-nrow(hh_200m) == sum(round(pop_200m$Men))
+nrow(hh_200m) == sum(ceiling(pop_200m$Men))
 
 hh_200m <- hh_200m %>% 
   left_join(
@@ -77,17 +119,141 @@ hh_200m_raster <- sdcSpatial::sdc_raster(
   max_risk = 1
 )
 
-plot(hh_200m_raster, value="sum", sensitive = FALSE)
-summary(pop_200m$Men)
-table(pop_200m$Men < 10)
+plot(hh_200m_raster, "count", col=pal, alpha = 0.85)
 
-pal <- rev(viridis(10))
-plot(hh_200m_raster, value = "sum", col = pal)
+# Sensitivity
+sensitivity_score(hh_200m_raster)
+prop.table(table(ceiling(pop_200m$Men) < 11))
 
+# remove sensitive cells
+hh_200m_rm <- remove_sensitive(hh_200m_raster)
+plot(hh_200m_rm, "count", col = pal, alpha = 0.85)
+
+length(hh_200m_rm$value$count)
 
 # quadtree
-hh_200m_qt <- protect_quadtree(hh_200m_raster)
-col <- c("#FDE333", "#BBDD38", "#6CD05E", "#00BE7D", "#00A890"
-         , "#008E98",  "#007094", "#185086", "#422C70", "#4B0055")
-plot(hh_200m_raster, "sum", col=col)
-plot(hh_200m_qt, "sum", col=col)
+hh_200m_qt <- protect_quadtree(hh_200m_raster, max_zoom = Inf)
+plot(hh_200m_qt, "count", col = pal, alpha = 0.85)
+#Different result from the one seen before: 
+#hypothesis: the function does not take the nesting of Inspire grid layers.
+#Ok for SDC if no use of Inspire grid at higher levels
+
+
+# smoothing
+hh_200m_sm_400 <- protect_smooth(hh_200m_raster, bw = 400)
+hh_200m_sm_1000 <- protect_smooth(hh_200m_raster, bw = 1000)
+plot(hh_200m_sm_400, "count", col = pal, alpha = 0.85)
+plot(hh_200m_sm_1000, "count", col = pal, alpha = 0.85)
+
+# The smoothing is quite low (several minutes to smooth only 14000 cells)
+# and the result is a little bit "imprecise": it's a way to protect the data,
+#but maybe we will make a lot of false positive (empty cells becoming non empty)
+# PROP: smoothing with btb package seems to be more refined
+# The btb package uses a quadratic kernel and a Diggle correction is implemented
+# to take into account - as far as possible - the existence of borders.
+# Presentation in Spatial Analysis Handbook, chapter 8
+# https://www.insee.fr/en/information/3635545
+
+pop_200m_df <- pop_grid_200m_sf %>% 
+  sf::st_drop_geometry() %>% 
+  dplyr::bind_cols(as.data.frame(sf::st_coordinates(centroides))[,1:2]) %>%
+  dplyr::select(x=X,y=Y,Ind,Men) 
+
+pop_200m_sm_400 <- btb::btb_smooth(pop_200m_df, sEPSG = "2975", iCellSize = 200, iBandwidth = 400)
+pop_200m_sm_1000 <- btb::btb_smooth(pop_200m_df, sEPSG = "2975", iCellSize = 200, iBandwidth = 1000)
+pop_200m_sm_2000 <- btb::btb_smooth(pop_200m_df, sEPSG = "2975", iCellSize = 200, iBandwidth = 2000)
+
+mapview(
+  pop_200m_sm_400, 
+  z = c("Men"),
+  at = qt,
+  lwd = 0,
+  color.regions = pal,
+  alpha.regions = 0.65,
+  na.color = NA,
+  layer.name = "Smooth - 400m"
+) +
+  mapview(
+    pop_200m_sm_1000, 
+    z = c("Men"),
+    at = qt,
+    lwd = 0,
+    color.regions = pal,
+    alpha.regions = 0.65,
+    na.color = NA,
+    layer.name = "Smooth - 1km",
+    hide = TRUE
+  ) +
+  mapview(
+    pop_200m_sm_2000, 
+    z = c("Men"),
+    at = qt,
+    lwd = 0,
+    color.regions = pal,
+    alpha.regions = 0.65,
+    na.color = NA,
+    layer.name = "Smooth - 2km",
+    hide = TRUE
+  ) +
+  mapview(
+    pop_200m %>% filter(Men < 11), 
+    lwd = 0.2,
+    color = "red",
+    alpha.regions = 0,
+    layer.name = "Sensitive"
+  )
+
+# Smoothing as SDC:
+# - a larger bw (comp. to the resolution) ensures more protection
+# - a too large bw will create a lot of non-sense cells
+# - In dense area => smoothing could be useful to tackle some rare pbs
+# - but with large maps (regional, state) mixing dense areas and rural areas,
+# - the smoothing could lead to a high loss of utility.
+
+# Case when smoothing could be helpful as a SDC method without loosing too much utility of the map
+# - maps in quite dense areas
+# - and in a quite homogoneic territory (not too many barriers creating too many no man's land)
+
+
+
+# Assess the utility of a map (and not only the loss of utility)
+# - are we creating a lot of non empty cells ? 
+# => difficult to defend when we can compare grid cells with satellite images for example
+# ex: smoothing 
+
+
+
+
+
+# library(quadtree)
+# 
+# r <- raster(
+#   xmn = min(hh_200m$x_sw),
+#   xmx = max(hh_200m$x_sw) + 200,
+#   ymn = min(hh_200m$y_sw),
+#   ymx = max(hh_200m$y_sw) + 200,
+#   res = 200,
+#   crs = 2975
+# )
+# 
+# rast <- raster::rasterize(x = hh_200m %>% sf::st_coordinates(), y = r)
+# values(rast) <- values(hh_200m_raster$value$count)
+# qt <- quadtree::quadtree(
+#   rast, 
+#   # split_threshold = 100, 
+#   split_method = "custom",
+#   split_fun = function(vals,args){
+#     if(all(is.na(vals))){
+#       return(FALSE)
+#     }else{
+#       return(min(vals, na.rm=TRUE) >= args$threshold) 
+#     }
+#   },
+#   split_args = list(threshold = 11),
+#   combine_method = "custom",
+#   combine_fun = function(vals,args) sum(vals[!is.na(vals)]),
+#   adj_type = "expand"
+# )
+# plot(qt, crop = TRUE, border_lwd = .3, na_col = NULL)
+
+
--- import_data_s3.sh
+++ import_data_s3.sh
@@ -1,2 +1,4 @@
 mc cp s3/julienjamme/public/Filosofi2017_carreaux_200m_reun.gpkg comparing_maps/data
 mc cp s3/julienjamme/public/Filosofi2017_carreaux_1km_reun.gpkg comparing_maps/data
+mc cp s3/julienjamme/public/commune_dep_974_2019.gpkg comparing_maps/data
+mc cp s3/julienjamme/public/Filosofi2017_carreaux_nivNaturel_reun.gpkg comparing_maps/data
